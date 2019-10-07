5344c5d52c56aa35b18aaafcafb0965fc563cbec
ericbanker12 <ericbanker12@gmail.com>
2019-10-07 14:22:40 -0500

    apply context to products in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// Contexts
+import ProductContext from './contexts/ProductContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -21,16 +24,13 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<ProductContext.Provider value={{products, addItem}} >
+				<Route
+					exact
+					path="/"
+					component={Products}
+				/>
+			</ProductContext.Provider>
 
 			<Route
 				path="/cart"
